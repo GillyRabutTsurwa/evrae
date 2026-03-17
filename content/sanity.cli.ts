e6de5aec85f9fba9dceebd989567c534d57a5635
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2026-03-17 16:22:56 -0400

    sanity: trying to make it work

--- content/sanity.cli.ts
+++ content/sanity.cli.ts
@@ -6,7 +6,7 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID,
     dataset: process.env.SANITY_STUDIO_DATASET,
   },
-  studioHost: "gilbertrabuttsurwa",
+  studioHost: "evræ",
   deployment: {
     appId: process.env.SANITY_STUDIO_APP_ID,
   },
