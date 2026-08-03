36e31aee2a37068b40ee163944ef6e4a50436b69
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2026-08-03 19:08:57 -0400

    removing the navigation component from the blog layout...

--- site/app/layouts/blog/index.vue
+++ site/app/layouts/blog/index.vue
@@ -1,40 +1,15 @@
 <template>
-    <Navigation>
-        <template #logo>
-            <LogoX />
-        </template>
-        <template #links>
-            <li class="navigation__list--item">
-                <NuxtLink to="/">Home</NuxtLink>
-            </li>
-            <li class="navigation__list--item">
-                <NuxtLink to="/blog">Blogs</NuxtLink>
-            </li>
-            <li class="navigation__list--item">
-                <NuxtLink to="/blog/authours/gilbert-rabut-tsurwa">About Me</NuxtLink>
-            </li>
-            <DevOnly>
-                <li class="navigation__list--item">
-                <NuxtLink to="/blog/uncensored">Uncensored Posts</NuxtLink>
-                </li>
-            </DevOnly>
-            <li class="navigation__list--item">
-                <NuxtLink to="/shop" target="_blank" rel="noreferrer noopener">Shop</NuxtLink>
-            </li>
-        </template>
-    </Navigation>
-    <div class="blogsettings">
-        <slot />
-    </div>
-    <Footer />
+  <div class="blogsettings">
+     <slot/>
+  </div>
+  <Footer/>
 </template>
 
 <style lang="scss">
 .blogsettings {
-
-    &,
-    & * {
-        font-family: "League Spartan", sans-serif !important;
-    }
+  &,
+  & * {
+    font-family: "League Spartan", sans-serif !important;
+  }
 }
 </style>
